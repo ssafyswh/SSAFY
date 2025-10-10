688879a81a0a58cecec38f3c13d11bc5b9c152e0
ssafyswh <swh4077@naver.com>
2025-10-10 16:46:33 +0900

    1010

--- memo/test.py
+++ memo/test.py
@@ -2,45 +2,56 @@ import sys
 sys.stdin = open('input.txt')
 # sys.stdout = open('output.txt', 'w', encoding='UTF-8')
 
-
 import sys
+from collections import deque
 
-s, n = map(int, input().split())
-nums = str(n)
-lcd = [[' '] * ((s + 2) * len(nums) + (len(nums) - 1)) for _ in range(2 * s + 3)]
-panel = [
-    {2, 3, 5, 6, 7, 8, 9, 0},
-    {4, 5, 6, 8, 9, 0},
-    {1, 2, 3, 4, 7, 8, 9, 0},
-    {2, 3, 4, 5, 6, 8, 9},
-    {2, 6, 8, 0},
-    {1, 3, 4, 5, 6, 7, 8, 9, 0},
-    {2, 3, 5, 6, 8, 9, 0}
-]
-for i in range(len(nums)):
-    start_x = (s + 3) * i
-    num = int(nums[i])
-    if num in panel[0]:
-        for x in range(start_x + 1, start_x + s + 1):
-            lcd[0][x] = '-'
-    if num in panel[1]:
-        for y in range(1, s + 1):
-            lcd[y][start_x] = '|'
-    if num in panel[2]:
-        for y in range(1, s + 1):
-            lcd[y][start_x + s + 1] = '|'
-    if num in panel[3]:
-        for x in range(start_x + 1, start_x + s + 1):
-            lcd[s + 1][x] = '-'
-    if num in panel[4]:
-        for y in range(s + 2, 2 * s + 2):
-            lcd[y][start_x] = '|'
-    if num in panel[5]:
-        for y in range(s + 2, 2 * s + 2):
-            lcd[y][start_x + s + 1] = '|'
-    if num in panel[6]:
-        for x in range(start_x + 1, start_x + s + 1):
-            lcd[2 * s + 2][x] = '-'
-for row in lcd:
-    print(''.join(row))
 
+def find_start():
+    for y in range(N):
+        for x in range(N):
+            if sea[y][x] == 9:
+                return y, x
+            
+            
+def find_fish():
+    cnt = 0
+    for y in range(N):
+        for x in range(N):
+            if sea[y][x] < size:
+                return True
+    return False
+
+
+def catch_fish():
+    global size, count
+    sy, sx = find_start()
+    q = deque([(0, sy, sx)])
+    while q:
+        time, ny, nx = q.popleft()
+        for d1, d2 in delta:
+            dy, dx = ny + d1, nx + d2
+            if not (0 <= dy < N and 0 <= dx < N):
+                continue
+            if sea[dy][dx] > size:
+                continue
+            if 1 <= sea[dy][dx] < size:
+                sea[dy][dx] = 0
+                count -= 1
+                if not count:
+                    size += 1
+                    count = size
+            q.append((time + 1, dy, dx))
+    
+    
+def solve():
+    while True:
+        if not find_fish():
+            break
+        
+    
+delta = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+N = int(input())
+size, count = 2, 2
+sea = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+        
